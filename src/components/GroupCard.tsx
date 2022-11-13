401befeb212d1739252a6ef91dfc1256ded32aa3
sid2033 <siddharthraju299@gmail.com>
2022-11-13 02:31:13 -0500

    feat: group page

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -1,16 +1,22 @@
 import type { Group } from "@prisma/client";
 import { Card } from "flowbite-react";
+import Link from "next/link";
 
 const GroupCard = ({ group }: { group: Group }) => {
   return (
-    <Card href="#">
-      <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-        {group.name}
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        {group.description}
-      </p>
-    </Card>
+    <Link
+      href={`/group/${group.id}`}
+      className="flex flex-col rounded-lg border border-gray-200 bg-white shadow-md hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+    >
+      <div className="flex h-full flex-col justify-center gap-4 p-6">
+        <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {group.name}
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {group.description}
+        </p>
+      </div>
+    </Link>
   );
 };
 
